5f57dbefdd984a0b453871cd90e093858af24745
Jarek Majka <vessekheal@gmail.com>
2021-11-27 22:14:12 +0100

    __str__ in models overriden

--- polls/models.py
+++ polls/models.py
@@ -5,8 +5,14 @@ class Question(models.Model):
     question = models.CharField(max_length=300)
     publication_date = models.DateTimeField('published date')
 
+    def __str__(self):
+        return self.question
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_desc = models.CharField(max_length=100)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_desc
