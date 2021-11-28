3f2b63376a8ee1bc484d28ba2605868cfdf5522a
Jarek Majka <vessekheal@gmail.com>
2021-11-28 14:02:29 +0100

    Strings f-stringed

--- polls/views.py
+++ polls/views.py
@@ -6,13 +6,13 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    return HttpResponse(f"You're looking at question {question_id}.")
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    response = f"You're looking at the results of question {question_id}."
+    return HttpResponse(response)
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    return HttpResponse(f"You're voting on question {question_id}.")
